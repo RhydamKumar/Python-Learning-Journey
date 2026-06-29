d87c4a0376fe84c6fc6253d52336ef996b6d62bd
RhydamKumar <rhydamkumar2004@gmail.com>
2026-06-29 11:15:40 +0530

    Add files via upload

---
+++ Python Fundamentals/Taking Inputs/02_taking_input_add.py
@@ -0,0 +1,9 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print(a + b)
+
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+print("Hello", name, ", you are", age, "years old.")
