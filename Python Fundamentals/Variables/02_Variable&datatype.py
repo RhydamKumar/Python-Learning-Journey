9d4aa58bfc9013b5d892bdb3f6f48f9462cb7fa6
RhydamKumar <rhydamkumar2004@gmail.com>
2026-06-29 11:14:35 +0530

    Add files via upload

---
+++ Python Fundamentals/Variables/02_Variable&datatype.py
@@ -0,0 +1,11 @@
+name = "Rhydam" #string
+print(name)
+print(type(name))       # Output: <class 'str'>
+
+age = 21 #integer
+print(age)
+print(type(age))  # Output: <class 'int'>
+
+height = 5.8 #float
+print(height)
+print(type(height))       # Output: <class 'float'>
