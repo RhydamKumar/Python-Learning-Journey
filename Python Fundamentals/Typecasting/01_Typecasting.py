6a3a5bc9ed3e7ffe68a9fcbcbf24b1fb2ceee9f4
RhydamKumar <rhydamkumar2004@gmail.com>
2026-06-29 11:15:18 +0530

    Add files via upload

---
+++ Python Fundamentals/Typecasting/01_Typecasting.py
@@ -0,0 +1,24 @@
+a = 20
+b = "27"
+c = 15
+g = 18.576
+
+# String to integer
+d = int(b)
+print(d)  # Output: 27
+print(type(d))  # Output: <class 'int'>
+
+# Integer to string
+e = str(a)
+print(e)  # Output: "20"
+print(type(e))  # Output: <class 'str'>
+
+# Integer to float
+f = float(c)
+print(f)  # Output: 15.0
+print(type(f))  # Output: <class 'float'>
+
+# Float to integer
+h = int(g)
+print(h)  # Output: 18
+print(type(h))  # Output: <class 'int'>
